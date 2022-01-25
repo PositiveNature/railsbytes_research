b1f7df99a419d87207a2cb0f9b02df61c2644a5f
PositiveNature <positive.nature.creative@gmail.com>
2022-01-25 04:38:43 +0100

    - add first cucumber feature test

---
+++ features/step_definitions/general_steps.rb
@@ -0,0 +1,9 @@
+Given('I am on the homepage') do
+  # pending # Write code here that turns the phrase above into concrete actions
+  visit root_path
+end
+
+Then('I should see {string}') do |string|
+  # pending # Write code here that turns the phrase above into concrete actions
+  page.should have_content(string)
+end
